977326a539dcdfff0198e6ec8cdc139c671d9b19
Stux <100985218+StuxVT@users.noreply.github.com>
2024-04-29 19:29:14 -0400

    enable stomping

--- RadMechSounds/RadMechSounds.cs
+++ RadMechSounds/RadMechSounds.cs
@@ -32,17 +32,18 @@ public class RadMechSounds : BaseUnityPlugin
         
         
         string dllPath = Instance.Info.Location;
-        string dllName = "YippeeMod.dll";
+        string dllName = "RadmechSounds.dll";
         string pluginPath = dllPath.TrimEnd(dllName.ToCharArray());
         string assetPath = pluginPath + "radmechsoundbundle";
         
         brainwashSounds = new AudioClip[5];
         AssetBundle val = AssetBundle.LoadFromFile(assetPath);
         
-        for (int i = 0; i < brainwashSounds.Length; i++)
-        {
-            brainwashSounds[i] = val.LoadAsset<AudioClip>("assets/lyla_bark.ogg");
-        }
+        brainwashSounds[0] = val.LoadAsset<AudioClip>("assets/RadMechStomp1.ogg");
+        brainwashSounds[1] = val.LoadAsset<AudioClip>("assets/RadMechStomp2.ogg");
+        brainwashSounds[2] = val.LoadAsset<AudioClip>("assets/RadMechStomp3.ogg");
+        brainwashSounds[3] = val.LoadAsset<AudioClip>("assets/RadMechStomp4.ogg");
+        brainwashSounds[4] = val.LoadAsset<AudioClip>("assets/lyla_bark.ogg");
         
         if (brainwashSounds[0] == null)
         { //  todo, check if all are null
